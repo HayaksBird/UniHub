1509b00ad4b69ed42a120739abd969206e50460c
Michael Shamshidov <mshamshidov22@ku.edu.tr>
2023-10-21 14:38:24 +0500

    update

--- node/controllers/redisController.js
+++ node/controllers/redisController.js
@@ -1,1 +1,21 @@
 const redisClient = require('../cache/redis-config')
+
+const getOrSetCache = (key, cb, exp) => {
+  return new Promise((resolve, reject) => {
+    redisClient.get(key, async (error, data) => {
+      if (error) return reject(error)
+      if (data != null) return resolve(JSON.parse(data))
+      const freshData = await cb();
+      redisClient.setex(key, exp, JSON.stringify(freshData))
+      resolve(freshData)
+    })
+  })
+}
+
+const updateCache = (key, valueToAdd) => {
+  //
+}
+
+
+
+module.exports = { getOrSetCache }
